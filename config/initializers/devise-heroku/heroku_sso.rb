c1bef5cd1973c2151ba00d6fa7430e622884fe6e
Daniel Morrison <daniel@collectiveidea.com>
2013-09-02 13:29:38 -0400

    Reorder SSO code to fail faster on old timestamp

--- config/initializers/devise-heroku/heroku_sso.rb
+++ config/initializers/devise-heroku/heroku_sso.rb
@@ -1,19 +1,17 @@
-Warden::Strategies.add(:heroku_sso_authenticable) do 
+Warden::Strategies.add(:heroku_sso_authenticable) do
   def valid?
     # try this strategy if correct params are present
-    return !(params[:token].nil? || params[:id].nil? || params[:timestamp].nil?)
+    !(params[:token].nil? || params[:id].nil? || params[:timestamp].nil?)
   end
 
   def authenticate! 
-    if (params[:token].nil? || params[:id].nil? || params[:timestamp].nil?)
-      fail!("invalid parameters")
-      return
-    end
+    fail!("invalid parameters") if !valid?
+    fail!("bad token") if params[:timestamp].to_i < (Time.now - 2*60).to_i
+
     pre_token = params[:id] + ':' + DeviseHeroku.sso_salt + ':' + params[:timestamp]
     token = Digest::SHA1.hexdigest(pre_token).to_s
 
     fail!("bad token") if token != params[:token]
-    fail!("bad token") if params[:timestamp].to_i < (Time.now - 2*60).to_i
 
     resource = DeviseHeroku.resource.find(params[:id])
     if resource
@@ -22,4 +20,4 @@ Warden::Strategies.add(:heroku_sso_authenticable) do
       fail!("not found")
     end
   end 
-end      
+end
