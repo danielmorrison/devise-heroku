bb64a428342b4931507a2f09a1d07f091472f594
Daniel Morrison <daniel@collectiveidea.com>
2013-09-02 22:12:10 -0400

    Skip :verify_authenticity_token It won't be expected in SSO.

--- app/controllers/devise_heroku/sso_controller.rb
+++ app/controllers/devise_heroku/sso_controller.rb
@@ -1,5 +1,5 @@
 class DeviseHeroku::SsoController < ApplicationController
-  
+  skip_before_filter :verify_authenticity_token
   before_filter "authenticate_#{DeviseHeroku.resource.to_s.parameterize.underscore}!".to_sym
 
   def login
